68375e641d045c51874db8428378a714a2904e1f
Mohamed <Eng_MohamedAllam@hotmail.com>
2018-06-14 04:03:07 +0300

    initial index setup

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 var express = require("express");
+var chalk = require("chalk");
+var debug = require("debug")("app");
+var morgan = require("morgan");
+var path = require("path");
 
 var app = express();
+app.use(morgan("tiny"));
+app.use(express.static(path.join(__dirname,"/public/")));
 
 app.get("/",function(req,res){
-    res.send("Hello from My Crowd funding App :) !");
+   // res.send("Hello from My Crowd funding App :) !");
+   res.sendFile(path.join( __dirname , "views/index.html"));
 })
 
 app.listen(3000,function(){
-    console.log("... App is up and Running on port 3000");
+   debug(`... App is up and Running on port ${chalk.green("3000")}`);
 })
