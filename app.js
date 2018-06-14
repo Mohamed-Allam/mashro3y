49e0e99a178a80f185d3745478c0fa3988b522a1
Mohamed <Eng_MohamedAllam@hotmail.com>
2018-06-14 19:41:55 +0300

    initial project structure

--- app.js
+++ app.js
@@ -1,18 +1,31 @@
-var express = require("express");
-var chalk = require("chalk");
-var debug = require("debug")("app");
-var morgan = require("morgan");
-var path = require("path");
+const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const path = require('path');
 
-var app = express();
-app.use(morgan("tiny"));
-app.use(express.static(path.join(__dirname,"/public/")));
+const port = process.env.PORT || 3001;
 
-app.get("/",function(req,res){
-   // res.send("Hello from My Crowd funding App :) !");
-   res.sendFile(path.join( __dirname , "views/index.html"));
-})
+const app = express();
+app.use(morgan('tiny'));
 
-app.listen(3000,function(){
-   debug(`... App is up and Running on port ${chalk.green("3000")}`);
-})
+app.use(express.static(path.join(__dirname, '/public/')));
+// app.use('/specificFolder', express.static(path.join(__dirname, '/specificFolder/')));
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
+
+
+app.get('/', (req, res) => {
+  // res.send('Hello from My Crowd funding App :) !');
+  // res.sendFile(path.join(__dirname, 'views/index.html'));
+  res.render('index',
+    {
+      title: 'Qatar Funds',
+      list: ['a', 'b']
+    }
+  );
+});
+
+app.listen(port, () => {
+  debug(`... App is up & Running on port ${chalk.green(port)}`);
+});
